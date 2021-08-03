e65c6eb079399811c2996e627a3c6bada2243702
tacettinkustu <tacettinkustu>
2021-08-03 19:54:28 +0300

    Add popup template to js file

--- src/comments/popup.js
+++ src/comments/popup.js
@@ -1,3 +1,39 @@
-export default function gotoCommentPage(showId) {
-  console.log(showId);
+import lookupShow from '../api/api.js';
+
+export default async function gotoCommentPage(showId) {
+  const showInfo = await lookupShow(showId);
+  const modal = document.querySelector('.modal');
+  modal.classList.add('active');
+
+  modal.innerHTML = `
+  <div
+          class="
+            modal-content
+            d-flex
+            flex-column
+            align-items-center
+            justify-content-center
+          "
+        >
+          <div class="d-flex flex-row w-100 justify-content-center">
+            <img
+              src="${showInfo.image.medium}"
+              alt="shows"
+              class="popup-image"
+            />
+            <div class="close">&times;</div>
+          </div>
+          <h3 class="popup-title my-4">${showInfo.name}</h3>
+          <div class="info d-flex flex-row my-2 w-75">
+            <div class="categories d-flex flex-column align-items-start w-50">
+              <h5>Genre : ${showInfo.genres[0]}</h5>
+              <h5>Language : ${showInfo.language}</h5>
+            </div>
+            <div class="categories d-flex flex-column align-items-start w-50">
+              <h5>Release Date : ${showInfo.premiered.split('-')[0]}</h5>
+              <h5>Rating : ${showInfo.rating.average}</h5>
+            </div>
+          </div>
+        </div>
+  `;
 }
