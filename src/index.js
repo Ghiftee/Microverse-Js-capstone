471cad862f14b1ea5f2c33e37dbdc6f67a6b1099
Ghiftee <gigibetine@gmail.com>
2021-08-03 09:22:03 +0100

    Create page header

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = 'Welcome to Microverse';
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
