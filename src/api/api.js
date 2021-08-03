e02de5a259ec8dda47a8196e4e4a2ea915f1c3e9
Ghiftee <gigibetine@gmail.com>
2021-08-03 14:06:28 +0100

    Create home page

---
+++ src/api/api.js
@@ -0,0 +1,10 @@
+const baseUrl = 'https://api.tvmaze.com';
+
+const lookupShow = async (showId) => {
+  const url = `${baseUrl}/shows/${showId}`;
+  let show = await fetch(url);
+  show = await show.json();
+  return show;
+};
+
+module.exports = { lookupShow };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './style.css';
+import { lookupShow } from './api/api.js';
+import { displayShows } from './home-page/page-display.js';
+
+const shows = [];
+
+document.addEventListener('DOMContentLoaded', async () => {
+  shows.push(
+    await lookupShow(139),
+    await lookupShow(140),
+    await lookupShow(141),
+    await lookupShow(145),
+    await lookupShow(143),
+    await lookupShow(144),
+  );
+  displayShows(shows);
+});
